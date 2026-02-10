f107ef81f25b502b5316c3e53bb756641c28a829
Roxanne Krause <rlutz1@unm.edu>
2026-02-10 06:50:29 -0700

    setting up to test P1 and alter what was done. this is an official check point of the absorption code not being changed.

--- HW1/P1.py
+++ HW1/P1.py
@@ -12,15 +12,19 @@ to the original one.
 """
 # from log_ada_absorbed_bias import AdalineGD, LogisticRegressionGD
 # from plotters import plot_decision_regions
+from helper_code.roxannes_abs_bias import AdalineGD, LogisticRegressionGD, Orig_AdalineGD, Orig_LogisticRegressionGD
 import numpy as np
-from matplotlib.colors import ListedColormap
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 
 # ===================================================================
 # SCRIPTING
 # ===================================================================
+
+# for consistency in training models so i don't lose my mind
+i = 1000
+e = 0.01
 
 s = 'https://archive.ics.uci.edu/ml/'\
     'machine-learning-databases/iris/iris.data'
@@ -38,11 +42,27 @@ y = np.where(y == "Iris-setosa", 0, 1) # setosa -> 0, versi 1
 # specifically sepal and petal lengths
 X = df.iloc[0:100, [0, 2]].values  
 
-# ada = AdalineGD(eta=0.01, n_iter=10) # note that eta needs to be small here!
-# ada.fit(X, y) # hand off the iris data and correct labels to learning algorithm
-# # plotting of the linearly separable decision regions.
-# plot_decision_regions(X, y, classifier=ada)
+# train absorbed ada
+ada_abs = AdalineGD(eta=e, n_iter=i) 
+ada_abs.fit(X, y) 
 
-# log = LogisticRegressionGD(eta=0.01, n_iter=100)
-# log.fit(X, y)
-# plot_decision_regions(X, y, classifier=log)
+# train absorbed log
+log_abs = LogisticRegressionGD(eta=e, n_iter=i)
+log_abs.fit(X, y)
+
+# train original ada
+log_orig = Orig_LogisticRegressionGD(eta=e, n_iter=i)
+log_orig.fit(X, y)
+
+# train original log
+ada_orig = Orig_AdalineGD(eta=e, n_iter=i)
+ada_orig.fit(X, y) 
+
+# i'm checking to make sure the weights and bias are the same between them
+print("ADA COMPARISON")
+print(f"absorbed weights (bias last): {ada_abs.w_}, orig weights: {ada_orig.w_}")
+print(f"absorbed bias: {ada_abs.w_[-1]}, orig bias: {ada_orig.b_}")
+
+print("LOG COMPARISON")
+print(f"absorbed weights (bias last): {log_abs.w_}, orig weights: {log_orig.w_}")
+print(f"absorbed bias: {log_abs.w_[-1]}, orig bias: {log_orig.b_}")
