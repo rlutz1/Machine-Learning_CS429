28b573d3642b9ce210f3f96d799f7fe9f0e5b73d
Roxanne Krause <rlutz1@unm.edu>
2026-02-13 11:25:34 -0700

    removing and reordering the models into better formatting because roxanne was being LAX earlier

---
+++ HW1/README.md
@@ -0,0 +1,4 @@
+# CS429 Assignment 1
+Assembled by: Roxanne Krause, Kurukulasuriya Leitan, Marnina Willard
+Submission Date: February 13, 2026
+
---
+++ HW1/helper_code/Absorbed_Adaline.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+"""
+MODIFIED ADALINE FOR ABSORBED BIAS
+
+baseline code is from the textbook for the adaline learning model.
+
+below are modifications to absorb the bias into the vectors w and x,
+comments mark the important changes.
+"""
+class AdalineGD:
+
+  def __init__(self, eta=0.01, n_iter=50, random_state=1):
+    self.eta = eta
+    self.n_iter = n_iter
+    self.random_state = random_state
+
+  def fit(self, X, y):
+    
+    rgen = np.random.RandomState(self.random_state)
+    self.w_ = rgen.normal(loc=0.0, scale=0.01,size=X.shape[1] + 1) # add one to size for bias
+    self.w_[-1] = np.float64(0.) # absorb b into w as the last element
+    self.losses_ = []
+
+    for i in range(self.n_iter): 
+      net_input = self.net_input(X)
+      output = self.activation(net_input)
+      errors = (y - output) 
+      self.w_[0:(X.shape[1])] += self.eta * 2.0 * X.T.dot(errors) / X.shape[0] # update the weights
+      self.w_[-1] += self.eta * 2.0 * errors.mean() # bias now in weight vector
+      loss = (errors**2).mean() 
+      self.losses_.append(loss)
+    return self
+  
+  # function to add ones to end of given samples
+  # in order to make use of the absorbed bias during
+  # net_input calculations.
+  def extend_samples(self, X):
+    ones = [[1]]* X.shape[0]
+    X = np.hstack((X, ones))
+    return X
+
+  def net_input(self, X):
+    """Calculate net input"""
+    X = self.extend_samples(X) # extend X with ones
+    return np.dot(X, self.w_) # w (dot) x, with bias absorbed.
+  
+  def activation(self, X):
+    """Compute linear activation"""
+    return X
+  
+  def predict(self, X):
+    """Return class label after unit step"""
+    return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
