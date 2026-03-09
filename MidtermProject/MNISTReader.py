2866af1d7715c364dcd8deebc3aca6cdfb826726
Roxanne Krause <rlutz1@unm.edu>
2026-03-09 13:10:03 -0600

    entering the skeleton for these 2 reader classes that can accomplish tasks 1 and 2 for both data sets

---
+++ MidtermProject/MNISTReader.py
@@ -0,0 +1,19 @@
+"""
+this class is for use in accomplishing tasks 1 and 2 for the MNIST dataset.
+this class should be used for tasks 3 and 4 for ease of training.
+
+Task 1:
+
+Download and import the training and test images from MNIST and Fashion MNIST. The imported
+data are required to be kept in NumPy array format. Complete the following tasks on both of the datasets.
+
+Task 2:
+
+Perform a data format transformation by flattening each image to a 1-D NumPy array. You may
+use the NumPy function reshape
+"""
+
+class MNISTReader:
+
+  def __init__():
+    pass
