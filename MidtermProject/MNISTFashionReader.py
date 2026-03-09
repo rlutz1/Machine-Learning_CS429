f9bf5df736a19a4ba9596ea5645e13195d34a22b
Roxanne Krause <rlutz1@unm.edu>
2026-03-09 14:23:25 -0600

    adding an image generator for sanity checking on datasets

--- MidtermProject/MNISTFashionReader.py
+++ MidtermProject/MNISTFashionReader.py
@@ -13,12 +13,50 @@ Perform a data format transformation by flattening each image to a 1-D NumPy arr
 use the NumPy function reshape
 """
 import os
-
-path = os.path.join(os.getcwd(), "datasets", "mnist-fashion")
+import matplotlib.pyplot as plt
+import idx2numpy
+import numpy as np
 
 # print(path)
 
 class MNISTFashionReader:
 
-  def __init__():
-    pass
+  # the path to the data sets -- assuming you're running from MidtermProject directory
+  # this is shared by ALL instances of this reader
+  path = os.path.join(os.getcwd(), "datasets", "mnist-fashion")
+
+  def __init__(self):
+    # read in the training images from idx files, convert to np
+    train_images = idx2numpy.convert_from_file(os.path.join(MNISTFashionReader.path, 'train-images-idx3-ubyte'))
+    # flatten to 1D np array
+    self.train_images = train_images.reshape(train_images.shape[0], 28 * 28) 
+
+    # read in the test images from idx files, convert to np
+    test_images = idx2numpy.convert_from_file(os.path.join(MNISTFashionReader.path, 't10k-images-idx3-ubyte'))
+    # flatten to 1D np array
+    self.test_images = test_images.reshape(test_images.shape[0], 28 * 28) 
+    
+    # grab training labels
+    self.train_labels = idx2numpy.convert_from_file(os.path.join(MNISTFashionReader.path, 'train-labels-idx1-ubyte'))
+    # grab testing labels
+    self.test_labels = idx2numpy.convert_from_file(os.path.join(MNISTFashionReader.path, 't10k-labels-idx1-ubyte'))
+    
+
+  def display_train_image(self, image_num):
+    if (image_num >= 0 and image_num < self.train_images.shape[0]):
+      print(self.train_images.size)
+      plt.imshow(self.train_images.reshape(self.train_images.shape[0], 28, 28)[image_num], cmap='gray')
+      plt.show()
+    else:
+      print(f"Image number must be on interval [0, {self.train_images.shape[0]})")
+
+
+    
+    
+   
+
+
+
+reader = MNISTFashionReader()
+
+reader.display_train_image(0)
