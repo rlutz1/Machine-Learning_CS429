8e0af0b9d1171ae64ace0527c2ecda2fe07a3909
Roxanne Krause <rlutz1@unm.edu>
2026-04-02 17:36:24 -0600

    adding the code from text book to sort data into pandas as process()

--- HW3/DataProcesser.py
+++ HW3/DataProcesser.py
@@ -1,6 +1,8 @@
 import tarfile
 import os
-import pyprind
+import pyprind 
+import pandas as pd
+import sys
 
 
 class DataProcessor:
@@ -13,17 +15,36 @@ class DataProcessor:
     extraction_path = os.path.join("data", ".extract") # extraction folder, to be ignored by git
 
     if not os.path.isdir(extraction_path):
-      print("Extraction path not made. Creating...")
+      print("Extraction path not made. Creating")
 
       # extraction script alone, better in a class method for use
       with tarfile.open(self._tar_file_path, 'r:gz') as tar:
         total_files = len(tar.getmembers()) # all files in folder
         for m in tar.getmembers(): 
-          print("extracting...", m.name, ", ", total_files, " left.") # for knowing it is still running instead of dead hang
+          print("extracting", m.name, ", ", total_files, " left.") # for knowing it is still running instead of dead hang
           tar.extract(m, path=extraction_path) # extract to folder
           total_files -= 1 # ease of reading only
     
     print("Extraction path already created. If want to re-extract, delete /data/.extract and re-run this extraction.")
 
+  def process(self):
+    extraction_path = os.path.join("data", ".extract") # extraction folder, to be ignored by git
+
+    labels = {'pos': 1, 'neg': 0}
+    pbar = pyprind.ProgBar(50000, stream=sys.stdout)
+    df = pd.DataFrame()
+
+    for s in ('test', 'train'):
+
+      for l in ('pos', 'neg'):
+        path = os.path.join(extraction_path, s, l)
+        for file in sorted(os.listdir(path)):
+          with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
+            txt = infile.read()
+          df = df.append([[txt, labels[l]]], ignore_index=True)
+          pbar.update()
+    df.columns = ['review', 'sentiment']
+
 dp = DataProcessor()
 dp.extract()
+dp.process()
