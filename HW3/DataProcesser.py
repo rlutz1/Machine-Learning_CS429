fb11f0c2b654fc58b0d1373ff64e1fef93b278f8
Roxanne Krause <rlutz1@unm.edu>
2026-04-02 17:30:13 -0600

    okay, that works a little better to at least be able to boot up data without running re-extraction again. simple check if the directory exists already

--- HW3/DataProcesser.py
+++ HW3/DataProcesser.py
@@ -1,13 +1,29 @@
 import tarfile
 import os
+import pyprind
 
-TAR_FILE = os.path.join(os.getcwd(), "data", "aclImdb_v1.tar.gz") # the tar file, TO BE stored by git
-EXTRACTION_PATH = os.path.join(os.getcwd(), "data", ".extract") # extraction folder, to be ignored by git
 
-# extraction script alone, better in a class method for use
-with tarfile.open(TAR_FILE, 'r:gz') as tar:
-  total_files = len(tar.getmembers()) # all files in folder
-  for m in tar.getmembers(): 
-    print("extracting...", m.name, ", ", total_files, " left.") # for knowing it is still running instead of dead hang
-    tar.extract(m, path=EXTRACTION_PATH) # extract to folder
-    total_files -= 1 # ease of reading only
+class DataProcessor:
+
+  def __init__(self):
+    self._tar_file_path = os.path.join("data", "aclImdb_v1.tar.gz") # relative path name
+
+  def extract(self):
+
+    extraction_path = os.path.join("data", ".extract") # extraction folder, to be ignored by git
+
+    if not os.path.isdir(extraction_path):
+      print("Extraction path not made. Creating...")
+
+      # extraction script alone, better in a class method for use
+      with tarfile.open(self._tar_file_path, 'r:gz') as tar:
+        total_files = len(tar.getmembers()) # all files in folder
+        for m in tar.getmembers(): 
+          print("extracting...", m.name, ", ", total_files, " left.") # for knowing it is still running instead of dead hang
+          tar.extract(m, path=extraction_path) # extract to folder
+          total_files -= 1 # ease of reading only
+    
+    print("Extraction path already created. If want to re-extract, delete /data/.extract and re-run this extraction.")
+
+dp = DataProcessor()
+dp.extract()
