670ba0a4ed0af8b3f3e3a80c79e806c93863e472
Roxanne Krause <rlutz1@unm.edu>
2026-04-03 13:23:49 -0600

    oops, forgot else, silly

--- HW3/DataProcesser.py
+++ HW3/DataProcesser.py
@@ -24,8 +24,10 @@ class DataProcessor:
           print("extracting", m.name, ", ", total_files, " left.") # for knowing it is still running instead of dead hang
           tar.extract(m, path=extraction_path) # extract to folder
           total_files -= 1 # ease of reading only
-    
-    print("Extraction path already created. If want to re-extract, delete /data/.extract and re-run this extraction.")
+
+      print("Extraction complete.")
+    else:
+      print("Extraction path already created. If want to re-extract, delete /data/.extract and re-run this extraction.")
 
   def process(self):
     extraction_path = os.path.join("data", ".extract") # extraction folder, to be ignored by git
