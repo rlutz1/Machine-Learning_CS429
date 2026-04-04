0c09486356ca27ed978a706b3a34d181afd3bb8a
Roxanne Krause <rlutz1@unm.edu>
2026-04-04 12:16:51 -0600

    okur, looking like the csv was created alright

--- HW3/DataProcesser.py
+++ HW3/DataProcesser.py
@@ -10,12 +10,12 @@ class DataProcessor:
 
   def __init__(self):
     self._tar_file_path = os.path.join("data", "aclImdb_v1.tar.gz") # relative path name
-    self._extraction_path = os.path.join("data", ".extract") # extraction folder, to be ignored by git
-    self._csv_path = os.path.join("data", ".csv") # path to write all reviews to as a csv
+    self._extraction_path = os.path.join("data", "extract") # extraction folder, to be ignored by git
+    self._csv_path = os.path.join("data", "csv_data.csv") # path to write all reviews to as a csv
 
   def extract(self):
 
-    if not os.path.isdir(self._extraction_path):
+    if not os.path.exists(self._csv_path):
       print("Extraction path not made. Creating")
 
       # extraction script alone, better in a class method for use
@@ -28,34 +28,48 @@ class DataProcessor:
 
       print("Extraction complete.")
     else:
-      print("Extraction path already created. If want to re-extract, delete /data/.extract and re-run this extraction.")
+      print("Extraction path already created. If want to re-extract, delete /data/extract and re-run this extraction.")
 
   def process(self, shuffle_seed=42):
 
-    labels = {'pos': 1, 'neg': 0} # positive vibes == 1, negative is 0
-    pbar = pyprind.ProgBar(50000, stream=sys.stdout) # set up a progress bar for sanity
-    df = pd.DataFrame() # data fram setup
+    if not os.path.isdir(self._extraction_path):
+      print("Extraction path not made. Creating")
 
-    for s in ('test', 'train'): # for both test and training extraction folders
+      labels = {'pos': 1, 'neg': 0} # positive vibes == 1, negative is 0
+      pbar = pyprind.ProgBar(50000, stream=sys.stdout) # set up a progress bar for sanity
+      df = pd.DataFrame({'review': [], 'sentiment': []}) # the dataframe start
 
-      for l in ('pos', 'neg'): # for each positive, negative in extraction folders
-        path = os.path.join(self._extraction_path, s, l) # build path to reviews
+      for s in ('test', 'train'): # for both test and training extraction folders
+
+        for l in ('pos', 'neg'): # for each positive, negative in extraction folders
+
+          path = os.path.join(os.getcwd(), self._extraction_path, "aclImdb", s, l) # build path to reviews
         
-        for file in sorted(path): # for each sorted file in path?
-          with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
-            txt = infile.read() # read in this review file
-          df = df.append([[txt, labels[l]]], ignore_index=True) # append row with review and pos/neg label
-          pbar.update() # update the progress bar
-    df.columns = ['review', 'sentiment'] # label the columns
+          with os.scandir(path) as files:
+            for file in files: # for each sorted file in path?
+              if file.is_file():
+                with open(os.path.join(path, file), 'r', encoding='utf-8') as infile:
+                  txt = infile.read() # read in this review file
 
-    # shuffle the data so it's not in complete order
-    np.random.seed(shuffle_seed)
-    df = df.reindex(np.random.permutation(df.index)) # shuffle up the rows
-    df.to_csv(self._csv_path, index=False, encoding="utf-8")# write to CSV
+                row = pd.DataFrame({"review": [txt], "sentiment": [labels[l]]})
+                df = pd.concat([df, row], ignore_index=True) # append row with review and pos/neg label
+                pbar.update() # update the progress bar
+
+      df.columns = ['review', 'sentiment'] # label the columns
+
+      # shuffle the data so it's not in complete order
+      np.random.seed(shuffle_seed)
+      df = df.reindex(np.random.permutation(df.index)) # shuffle up the rows
+      df.to_csv(self._csv_path, index=False, encoding="utf-8")# write to CSV
 
 
-    # sanity checking
-    df.head(3)
+      # sanity checking
+      df.head(3)
+    else:
+      print("CSV data already created. If want to recreate, delete data/csv_data.csv and rerun this process.")
+
+
+
 
 dp = DataProcessor()
 dp.extract()
