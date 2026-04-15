721337662cefd2d72a3b665748d8b20d8576269f
Roxanne Krause <rlutz1@unm.edu>
2026-04-15 13:56:39 -0600

    setting up method sigs to get the rest underway

--- HW4/DataProcessor.py
+++ HW4/DataProcessor.py
@@ -22,7 +22,7 @@ USAGE NOTES
 
 class DataProcessor:
 
-  def __init__(self):
+  def __init__(self, data_grab=True, clean=True):
 
     # set up key pair of readable name to stock symbol
     self.company_symbols = {
@@ -32,32 +32,63 @@ class DataProcessor:
       "Google": "GOOG" # GOOGL is also an option. not literate enough to know the distict diff yet
     }
 
-    # testing
-    # print(" ".join(list(self.company_symbols.values())))
+    # initialize the training and testing arrays
+    self.X_train = np.array([])
+    self.y_train = np.array([])
+    self.X_test = np.array([])
+    self.y_test = np.array([])
 
     # select a time frame/period to shoot for
-    # self.time_frame = "1y" # 1 year to start
-    self.time_frame = "1mo" # testing
+    self.time_frame = "1y" # 1 year to start
+    # self.time_frame = "1mo" # testing
 
     # path for the raw data dump, NO cleaning
     self.raw_csv_dir = os.path.join("data", "raw")
 
-    # encapsulate the data pull from yahoo finance
-    self._get_init_data()
+    # encapsulate the data pull from yahoo finance, overwriting the current csv
+    if data_grab:
+      self._get_raw_data()
+
+    # clean the data, overwriting the current csv
+    if clean:
+      self._clean()
+
+    # split the data into test and train
+    self._split()
+    # convenience wrapper to remember to convert np to pytorch tensor
+    self._to_tensor()
 
 
   # method to pull the raw data initially
-  def _get_init_data(self):
+  def _get_raw_data(self):
     for symbol in self.company_symbols.values():
       print(f"pinging for {symbol} data over {self.time_frame}")
       df = yf.download(symbol, period=self.time_frame)
       print(df[:3]) # print first 3 things
       df.to_csv(os.path.join(self.raw_csv_dir, f"{symbol}_raw.csv"), index=False, encoding="utf-8")# write ALL to a csv
+
+  # clean the raw text using various methods
+  def _clean(self):
+    pass
+
+  # split into test and training sets.
+  # this will create M length windows that overlap of all the data, 
+  # and then setting the "true" label to the next closing price
+  def _split(self):
+    pass
+
+  # convenience wrapper method to convert the split
+  # sets into pytorch tensors after splitting
+  def _to_tensor(self):
+    pass
     
 
 # ===========================================
 # TESTING
 
-dp = DataProcessor()
+dp = DataProcessor(
+  data_grab=True, # TRUE: grab the raw data from yahoo finance and overwrite the existing CSVs
+  clean=True # TRUE: clean the raw data and overwrite the existing CSVs
+  ) # TODO set to false before any usage so they don't have to repull crap 
 
 # ===========================================
