cde3c7b1b8d6daced8b80400c826064fe0a54d15
Roxanne Krause <rlutz1@unm.edu>
2026-04-15 07:47:35 -0600

    setting up the DP (lol)

---
+++ HW4/DataProcessor.py
@@ -0,0 +1,24 @@
+"""
+Task 1:
+Find 4 stocks from Yahoo Finance and download their historical prices in one
+or several years, e.g., from 02/01/2018 to 01/31/2021. 
+You may use the same time window for all stocks.
+You should appropriately preprocess your data using scikit-learn and keep the result as the PyTorch tensors.
+Randomly split your data to a training set (80%) and a test set (20%)
+"""
+# imports
+import numpy as np
+import pandas as pd
+import os
+
+"""
+USAGE NOTES
+
+
+"""
+
+
+class DataProcessor:
+
+  def __init__():
+    pass # TODO
