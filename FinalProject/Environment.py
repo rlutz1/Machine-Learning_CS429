b852b98a434136882ba92fc05dacc12740a1aa67
kleitan <kleitan@unm.edu>
2026-04-30 19:07:55 -0600

    small stuff

--- FinalProject/Environment.py
+++ FinalProject/Environment.py
@@ -27,10 +27,10 @@ N_ACTIONS = 4
 
 # Changes needed to move in that direction
 DELTA = {
-    LEFT: (-1,  0),
-    RIGHT: ( 1,  0),
+    LEFT: (-1, 0),
+    RIGHT: ( 1, 0),
     UP: ( 0, -1),
-    DOWN: ( 0,  1),
+    DOWN: ( 0, 1),
 }
 
 
