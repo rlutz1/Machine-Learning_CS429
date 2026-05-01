eda6cfafb943c671956774755915f540d8e35399
Roxanne Krause <rlutz1@unm.edu>
2026-05-01 11:59:00 -0600

    probing environ, still sussing out what the broad strokes are needed for a generic agent

--- FinalProject/Agent.py
+++ FinalProject/Agent.py
@@ -10,6 +10,7 @@ is reached and a very negative reward is returned when an obstacle is reached
 # imports
 import numpy as np
 from MapCompressor import MapCompressor
+from Environment import Environment
 
 class Agent:
 
@@ -21,8 +22,29 @@ class Agent:
     # dimension 2: y coords (map_height)
     # dimension 3: actions (U, D, L, R) (4)
     self.Q_TABLE = np.zeros((map_width, map_height, 4))
-    print(self.Q_TABLE.shape)
+    # print(self.Q_TABLE.shape)
     self.environment = environment
+
+  # interaction with environment, update Qtable
+  def interact(self, 
+               curr_state: tuple,        # next coordinate (x, y)
+               next_action: int,         # next action {0, 1, 2, 3} -> (LEFT / RIGHT / UP / DOWN)
+               strategy: str = "S1" # strategy to use
+               ):
+    # probe the environment
+    next_state, reward, done = self.environment.step(state=curr_state, action=next_action, strategy=strategy)
+    if not done:
+      self.update_Q_TABLE(state=next_state, action=action, reward=reward)
+
+  # is this where policy eval/update goes? unclear... hold on
+  def update_Q_TABLE(self,  state: tuple, action: int, reward:float):
+    x = state[0]
+    y = state[1]
+    self.Q_TABLE[x][y][action] = self.algo()
+
+  # redefine this with sarsa update or QLearn?
+  def algo(self):
+    pass
 
 # ==========================================================================================================
 # TESTING
@@ -31,7 +53,9 @@ class Agent:
 mc = MapCompressor()
 im = mc.compress(mc.MAP_1_PATH)
 
-agent = Agent(im.shape[0], im.shape[1])
+environment = Environment(im, (0, 0)) # testing only
+
+agent = Agent(environment, im.shape[0], im.shape[1]) # agent 
 # plt.imshow(im)
 # plt.colorbar()
 # plt.show()
