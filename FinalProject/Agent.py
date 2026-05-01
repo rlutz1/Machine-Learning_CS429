bb5cb664683319e9a62033b2a43dd5b0324da818
Roxanne Krause <rlutz1@unm.edu>
2026-05-01 11:14:03 -0600

    simply setting up generic agent for task 3

---
+++ FinalProject/Agent.py
@@ -0,0 +1,14 @@
+"""
+BUILDING THE AGENT
+
+You are required to write a Python class for the agent which keeps a
+Q-table. At least a function for the interaction with an environment should be implemented. It performs an
+interaction with an environment and updates the Q-table. As it is explained in the class, the Q-table should
+be a 3-D matrix. S1 can be a naive strategy in which a very high reward is returned when the target position
+is reached and a very negative reward is returned when an obstacle is reached
+"""
+
+class Agent:
+
+  def __init__():
+    pass
