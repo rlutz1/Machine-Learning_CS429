cbb93e5f2e06f71b268c8d62268e7d664ce10c36
Roxanne Krause <rlutz1@unm.edu>
2026-05-01 11:24:25 -0600

    setting up Q table

--- FinalProject/Agent.py
+++ FinalProject/Agent.py
@@ -7,8 +7,32 @@ interaction with an environment and updates the Q-table. As it is explained in t
 be a 3-D matrix. S1 can be a naive strategy in which a very high reward is returned when the target position
 is reached and a very negative reward is returned when an obstacle is reached
 """
+# imports
+import numpy as np
+from MapCompressor import MapCompressor
 
 class Agent:
 
-  def __init__():
+  
+
+  def __init__(self, map_width=40, map_height=40):
+    # self.num_coordinates = map_width * map_height # for ease of use
+    # make a 3d matrix: 
+    # dimension 1: x coords (map_width)
+    # dimension 2: y coords (map_height)
+    # dimension 3: actions (U, D, L, R) (4)
+    self.Q_TABLE = np.zeros((map_width, map_height, 4))
+    print(self.Q_TABLE.shape)
     pass
+
+# ==========================================================================================================
+# TESTING
+# ==========================================================================================================
+
+mc = MapCompressor()
+im = mc.compress(mc.MAP_1_PATH)
+
+agent = Agent(im.shape[0], im.shape[1])
+# plt.imshow(im)
+# plt.colorbar()
+# plt.show()
